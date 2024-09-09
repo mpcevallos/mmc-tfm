157f8d63a52ceb931bab36f8138f4d2d7144f539
mpcevallos <mpcevallos@gmail.com>
2024-09-09 18:39:04 -0500

    test css

--- main.jsx
+++ main.jsx
@@ -5,7 +5,7 @@ import NavBar from "./src/assets/components/NavBar.jsx";
 import Login from "./src/assets/components/Login.jsx";
 import { useEffect } from "react";
 import App from "./App.jsx";
-import "./src/assets/styles/styles.css";
+import "https://drive.google.com/file/d/1CcykTP4Km6pZ7-it7z-z8qWK8Bam2Kwa/view?usp=drive_link";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
